f9b4bf3fff6efc9b528d60ccab0a3e2101679edb
phantomomm <ommsaiiitr123gmail.com>
2026-08-03 23:52:26 +0530

    08/07/26

---
+++ userdefineddatastructure.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Student{
+  public:
+   double gpa;
+   int age;
+   string name;
+};
+
+int main()
+{
+   Student s;
+   s.gpa = 7.2;
+   s.age = 45;
+   s.name = "Omm";
+}
