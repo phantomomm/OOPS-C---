f2652c11ea7ef5814028351c7a121e0c70b37117
phantomomm <ommsaiiitr123gmail.com>
2026-08-05 01:04:16 +0530

    05/08/26

--- consrructor.cpp
+++ consrructor.cpp
@@ -15,8 +15,13 @@ public:
        rno = r;
        gpa = g;   // Parameterised constructor is just a function with no return type.
    }
-};
 
+   Student(string s,int r){  
+    name = s;
+    rno = r;      //Multiple contructors too can be created, while changing number of arguments.
+   }
+};
+  
 void print(Student s)
 {
    cout << s.gpa << " " <<s.rno << " " << s.name;
@@ -35,6 +40,15 @@ int main()
    s2.name = "dev";
    s2.rno = 45;
    s2.gpa = 8.9;  // if a constructor is alr created then this wont get executed unless the default is created specifically
+
+   Student s3("Alex",37);
+   s3.gpa = 7.9;
+
+   Student s4 = s1;
+   s4.name = "Pratik"; //Deep copy , gets overwritten in such case
+
+   Student s5(s1);
+   s5.name = "Patrick"; //Deep copy
    
    print(s1);
 }
